78b84ce2b228041eeabf3ad71c7621df006db4a7
Eric Schrock <eric.schrock@delphix.com>
2019-11-15 23:28:57 -0500

    remote commit operations (#3)

--- server/src/main/kotlin/io/titandata/remote/s3web/server/S3WebRemoteServer.kt
+++ server/src/main/kotlin/io/titandata/remote/s3web/server/S3WebRemoteServer.kt
@@ -1,8 +1,14 @@
 package io.titandata.remote.s3web.server
 
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import io.titandata.remote.RemoteOperation
 import io.titandata.remote.RemoteServer
 import io.titandata.remote.RemoteServerUtil
+import java.io.IOException
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 /**
  * The S3 web provider is a very simple provider for reading commits created by the S3 provider. It's primary purpose is
@@ -17,6 +23,8 @@ import io.titandata.remote.RemoteServerUtil
 class S3WebRemoteServer : RemoteServer {
 
     internal val util = RemoteServerUtil()
+    internal val gson = GsonBuilder().create()
+    internal val http = OkHttpClient()
 
     override fun getProvider(): String {
         return "s3web"
@@ -38,12 +46,59 @@ class S3WebRemoteServer : RemoteServer {
         return parameters
     }
 
-    override fun getCommit(remote: Map<String, Any>, parameters: Map<String, Any>, commitId: String): Map<String, Any>? {
-        throw NotImplementedError()
+    /**
+     * Fetch a file from the given remote, returning as a response.
+     */
+    internal fun getFile(remote: Map<String, Any>, path: String): Response {
+        val url = remote["url"] as String
+        val request = Request.Builder().url("$url/$path").build()
+        return http.newCall(request).execute()
+    }
+
+    /**
+     * Get all commits stored in the main metadata file. Since this is the only way we can get the metadata of
+     * a commit we use it for both listing commits and fetching individual commits. It is not particuarly efficient.
+     */
+    internal fun getAllCommits(remote: Map<String, Any>): List<Pair<String, Map<String, Any>>> {
+        val response = getFile(remote, "titan")
+        val url = remote["url"] as String
+        val body = if (response.isSuccessful) {
+            response.body!!.string()
+        } else if (response.code == 404) {
+            ""
+        } else {
+            throw IOException("failed to get $url/titan, error code ${response.code}")
+        }
+
+        val ret = mutableListOf<Pair<String, Map<String, Any>>>()
+
+        for (line in body.split("\n")) {
+            if (line != "") {
+                val result: Map<String, Any> = gson.fromJson(line, object : TypeToken<Map<String, Any>>() {}.type)
+                val id = result.get("id")
+                val properties = result.get("properties")
+                if (id != null && properties != null) {
+                    id as String
+                    @Suppress("UNCHECKED_CAST")
+                    properties as Map<String, Any>
+                    ret.add(id to properties)
+                }
+            }
+        }
+
+        return ret
     }
 
     override fun listCommits(remote: Map<String, Any>, parameters: Map<String, Any>, tags: List<Pair<String, String?>>): List<Pair<String, Map<String, Any>>> {
-        throw NotImplementedError()
+        val commits = getAllCommits(remote)
+        val matching = commits.filter { util.matchTags(it.second, tags) }
+        return util.sortDescending(matching)
+    }
+
+    override fun getCommit(remote: Map<String, Any>, parameters: Map<String, Any>, commitId: String): Map<String, Any>? {
+        val commits = getAllCommits(remote)
+        val match = commits.filter { it.first == commitId }.firstOrNull()
+        return match?.second
     }
 
     override fun endOperation(operation: RemoteOperation, isSuccessful: Boolean) {
